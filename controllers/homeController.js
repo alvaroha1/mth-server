f7599ee32d56011033fefd6723c3e1c4f0eb25e3
Peter Schindler <petertasschindler@gmail.com>
2018-10-17 15:59:15 +0200

    initializing pagination

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,17 @@
 const Home = require('../models/homeModel');
 const { avgCalc, createFilter } = require('./helpers');
 
+const homesPerPage = 20;
+const pageNumber = 3;
+
 exports.getAllHomes = async (req, res) => {
   const queryFilter = createFilter(req);
   const allHomes = await Home.find(queryFilter);
+  const start = homesPerPage * (pageNumber - 1);
+  const end = homesPerPage * pageNumber - 1;
+  const responseHomes = allHomes.slice(start, end);
   const avgM2Price = avgCalc(allHomes);
-  const allHomesFormatted = allHomes.map((obj) => {
+  const allHomesFormatted = responseHomes.map((obj) => {
     const formattedObj = {};
     formattedObj.thumbnail = obj.thumbnail;
     formattedObj.price = obj.price;
@@ -21,6 +27,12 @@ exports.getAllHomes = async (req, res) => {
     return formattedObj;
   });
   const response = {
+    centerLatitude: 44.427967,
+    centerLongitude: 8.849993,
+    radius: 10000,
+    page: pageNumber,
+    totalPages: Math.ceil(allHomes.length / homesPerPage),
+    totalResults: allHomes.length,
     averagePricePerSquareMeter: avgM2Price,
     homesList: allHomesFormatted,
   };
